fa062b3b331a750096bb204de62a019da8a12a1b
awesomearjun <iamawesomearjun@gmail.com>
2022-12-21 23:31:35 -0500

    first commit

---
+++ learning-arduino.ino
@@ -0,0 +1,23 @@
+const int buttonPin = 2;
+const int ledPin = 3;
+
+int buttonState = 0;
+
+void setup() {
+	pinMode(ledPin, OUTPUT);
+	pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+	buttonState = digitalRead(buttonPin);
+
+	Serial.println(buttonState);
+
+	if(buttonState == LOW) {
+		digitalWrite(ledPin, HIGH);
+	} else {
+		digitalWrite(ledPin, LOW);
+	}
+
+	delay(200);
+}
