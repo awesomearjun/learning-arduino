1a7d41189271664f63299a06faa50a73a1dad980
awesomearjun <iamawesomearjun@gmail.com>
2022-12-23 11:24:38 -0500

    Add support for RGB

--- learning-arduino.ino
+++ learning-arduino.ino
@@ -1,30 +1,46 @@
-const int buttonPin = 2;
-const int ledPin = 3;
+const int redButton = 1;
+const int greenButton = 2;
+const int blueButton = 3;
 
-int buttonState = 0;
-bool flag = false;
+const int redPin = 8;
+const int greenPin = 10;
+const int bluePin = 9;
+
+const float incrementor = 0.1f;
+
+int redState = 0;
+int greenState = 0;
+int blueState = 0;
+
+float red;
+float green;
+float blue;
 
 void setup() {
-	pinMode(ledPin, OUTPUT);
-	pinMode(buttonPin, INPUT);
+	pinMode(redButton, INPUT);
+	pinMode(greenButton, INPUT);
+	pinMode(blueButton, INPUT);
+
+	pinMode(redPin, OUTPUT);
+	pinMode(greenPin, OUTPUT);
+	pinMode(bluePin, OUTPUT);
+	Serial.begin(9600);
 }
 
 void loop() {
-	buttonState = digitalRead(buttonPin);
+	redState = digitalRead(redButton);
+	greenState = digitalRead(greenButton);
+	blueState = digitalRead(blueButton);
 
-	Serial.println(buttonState);
-
-	if(!flag && buttonState == LOW) {
-		flag = true;
-	} else if (flag && buttonState == LOW) {
-		flag = false;
+	if(redState == LOW) {
+		red += incrementor;
+	} else if(greenState == LOW) {
+		green += incrementor;
+	} else if (blueState == LOW) {
+		blue += incrementor;
 	}
 
-	if(flag) {
-		digitalWrite(ledPin, HIGH);
-	} else {
-		digitalWrite(ledPin, LOW);
-	}
-
-	delay(200);
+	digitalWrite(redPin, red);
+	digitalWrite(greenPin, green);
+	digitalWrite(bluePin, blue);
 }
