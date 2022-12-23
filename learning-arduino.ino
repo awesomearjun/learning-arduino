9892d5149d398034d4b4905eba1408cafffec1e1
awesomearjun <iamawesomearjun@gmail.com>
2022-12-23 10:29:35 -0500

    Add flag

--- learning-arduino.ino
+++ learning-arduino.ino
@@ -2,6 +2,7 @@ const int buttonPin = 2;
 const int ledPin = 3;
 
 int buttonState = 0;
+bool flag = false;
 
 void setup() {
 	pinMode(ledPin, OUTPUT);
@@ -13,7 +14,13 @@ void loop() {
 
 	Serial.println(buttonState);
 
-	if(buttonState == LOW) {
+	if(!flag && buttonState == LOW) {
+		flag = true;
+	} else if (flag && buttonState == LOW) {
+		flag = false;
+	}
+
+	if(flag) {
 		digitalWrite(ledPin, HIGH);
 	} else {
 		digitalWrite(ledPin, LOW);
