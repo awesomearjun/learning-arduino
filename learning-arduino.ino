0a633f47a64ba897251a97cb9985026eb3cec82d
awesomearjun <iamawesomearjun@gmail.com>
2022-12-23 20:55:25 -0500

    Make ultrasonic sensor

--- learning-arduino.ino
+++ learning-arduino.ino
@@ -1,85 +1,47 @@
-const int redButton = 1;
-const int greenButton = 2;
-const int blueButton = 3;
-const int flagButton = 13;
+const int trigPin = 12;
+const int echoPin = 13;
+const float speedOfSound = 0.034f;
 
-const int redPin = 8;
-const int greenPin = 10;
-const int bluePin = 9;
+long duration;
+int distance;
 
-const int greenLED = 7;
-const int redLED = 6;
-
-bool flag = true;
-
-const float incrementor = 1.0f;
-
-int redState = 0;
-int greenState = 0;
-int blueState = 0;
-int flagState = 0;
-
-float red = 0.0f;
-float green = 0.0f;
-float blue = 0.0f;
+const int red = 4;
+const int yellow = 5;
+const int green = 6;
 
 void setup() {
-	pinMode(redButton, INPUT);
-	pinMode(greenButton, INPUT);
-	pinMode(blueButton, INPUT);
-
-	pinMode(greenLED, OUTPUT);
-	pinMode(redLED, OUTPUT);
-
-	pinMode(redPin, OUTPUT);
-	pinMode(greenPin, OUTPUT);
-	pinMode(bluePin, OUTPUT);
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+	pinMode(red, OUTPUT);
+	pinMode(yellow, OUTPUT);
+	pinMode(green, OUTPUT);
 	Serial.begin(9600);
 }
 
 void loop() {
-	redState = digitalRead(redButton);
-	greenState = digitalRead(greenButton);
-	blueState = digitalRead(blueButton);
-	flagState = digitalRead(flagButton);
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
 
-	if(flagState == LOW) {
-		flag = !flag;
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+
+	duration = pulseIn(echoPin, HIGH);
+	distance = duration * speedOfSound / 2;
+
+	if(distance < 11) {
+		digitalWrite(red, HIGH);
+		digitalWrite(green, LOW);
+		digitalWrite(yellow, LOW);
+	} else if(distance < 36) {
+		digitalWrite(yellow, HIGH);
+		digitalWrite(red, LOW);
+		digitalWrite(green, LOW);
+	} else if(distance > 60) {
+		digitalWrite(green, HIGH);
+		digitalWrite(yellow, LOW);
+		digitalWrite(red, LOW);
 	}
 
-	if(flag) {
-		digitalWrite(greenLED, HIGH);
-		digitalWrite(redLED, LOW);
-	} else {
-		digitalWrite(redLED, HIGH);
-		digitalWrite(greenLED, LOW);
-	}
-
-	if(redState == LOW && flag && red < 255) {
-		red += incrementor;
-	} else if(greenState == LOW && flag && green < 255) {
-		green += incrementor;
-	} else if (blueState == LOW && flag && blue < 255) {
-		blue += incrementor;
-	}
-
-	if(redState == LOW && !flag && red > 0) {
-		red -= incrementor;
-	} else if(greenState == LOW && !flag && green > 0) {
-		green -= incrementor;
-	} else if (blueState == LOW && !flag && blue > 0) {
-		blue -= incrementor;
-	}
-
-	if(red > 255) {
-		red = 255;
-	} else if(green > 255) {
-		green = 255;
-	} else if(blue > 255) {
-		blue = 255;
-	}
-
-	digitalWrite(redPin, red);
-	digitalWrite(greenPin, green);
-	digitalWrite(bluePin, blue);
+	delay(200);
 }
